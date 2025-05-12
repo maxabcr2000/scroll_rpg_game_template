9d0da431d8637b914e9448a131e94b1231a812b9
maxabcr2000 <maxabcr2000@gmail.com>
2025-05-12 19:19:31 +0800

    Implement a basic player movement system

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,15 @@ use bevy_sprite3d::prelude::*;
 #[derive(States, Hash, Clone, PartialEq, Eq, Debug, Default)]
 enum GameState { #[default] Loading, Ready }
 
+#[derive(Component, PartialEq, Eq)]
+enum PlayerState {
+    Idle,
+    Moving,
+}
+
+#[derive(Component)]
+struct Player;
+
 #[derive(Resource, Default)]
 struct ImageAssets {
     image: Handle<Image>,               // the `image` field here is only used to query the load state, lots of the
@@ -37,6 +46,7 @@ fn main() {
 
         // every frame, animate the sprite
         .add_systems(Update, animate_sprite.run_if(in_state(GameState::Ready)))
+        .add_systems(Update, player_movement_system.run_if(in_state(GameState::Ready)))
 
         .insert_resource(ImageAssets::default())
         .run();
@@ -74,7 +84,9 @@ fn setup(
             // pivot: Some(Vec2::new(0.5, 0.5)),
             ..default()
     }.bundle_with_atlas(&mut sprite_params, texture_atlas))
-    .insert(AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)));
+    .insert(AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)))
+    .insert(Player)
+    .insert(PlayerState::Idle);
 
     // -----------------------------------------------------------------------
 }
@@ -82,14 +94,47 @@ fn setup(
 
 fn animate_sprite(
     time: Res<Time>,
-    mut query: Query<(&mut AnimationTimer, &mut Sprite3d)>,
+    mut query: Query<(&mut AnimationTimer, &mut Sprite3d, &PlayerState)>,
 ) {
-    for (mut timer, mut sprite_3d) in query.iter_mut() {
-        timer.tick(time.delta());
-        if timer.just_finished() {
-            let length = sprite_3d.texture_atlas_keys.as_ref().unwrap().len();
-            let atlas = sprite_3d.texture_atlas.as_mut().unwrap();
-            atlas.index = (atlas.index + 1) % length;
+    for (mut timer, mut sprite_3d, state) in query.iter_mut() {
+        match state {
+            PlayerState::Moving => {
+                timer.tick(time.delta());
+                if timer.just_finished() {
+                    let length = sprite_3d.texture_atlas_keys.as_ref().unwrap().len();
+                    let atlas = sprite_3d.texture_atlas.as_mut().unwrap();
+                    atlas.index = (atlas.index + 1) % length;
+                }
+            }
+            PlayerState::Idle => {
+                // 重設為第0幀
+                let atlas = sprite_3d.texture_atlas.as_mut().unwrap();
+                atlas.index = 0;
+            }
+        }
+    }
+}
+
+fn player_movement_system(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&mut Transform, &mut PlayerState), With<Player>>,
+    time: Res<Time>,
+) {
+    for (mut transform, mut state) in query.iter_mut() {
+        let mut direction = 0.0;
+        if keyboard_input.pressed(KeyCode::ArrowLeft) {
+            direction -= 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::ArrowRight) {
+            direction += 1.0;
+        }
+
+        if direction != 0.0 {
+            transform.translation.x += direction * 0.5 * time.delta_secs();
+            transform.scale.x = direction.signum();
+            *state = PlayerState::Moving;
+        } else {
+            *state = PlayerState::Idle;
         }
     }
 }
